5368849607cd8ff0171d727c60de8a33ee372374
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2025-07-31 18:17:49 +0530

    "How to print and take input/output in buffer"

---
+++ print/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 25
+	name := "Omkar"
+
+	fmt.Println(age, name)
+
+	fmt.Printf("Name: %s, Age: %d\n", name, age)
+
+}
---
+++ user_input/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	name, _ := reader.ReadString('\n') // reads input until newline
+	fmt.Println("Hello,", name)
+}
