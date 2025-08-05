8fc08aaf3d73af64b3bc2c64bee1d428b8dea709
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2025-08-05 11:48:50 +0530

    functions in golang

---
+++ Functions/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// func SimpleFunction() {
+// 	fmt.Println("Simple function")
+// }
+
+//some ways and syntax of adding in a function
+
+func adding1(a, b, c int) int {
+	return a + b + c
+}
+
+func adding2(a int, b int, c int) int {
+	return a + b + c
+}
+
+
+
+func main() {
+	fmt.Println("Function learning in go")
+
+
+
+}
